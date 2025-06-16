542392f10a423cfff25ce2d8a0ac54ae296c34d4
Jhordan Caceres <jhordancaceres25@gmail.com>
2025-06-16 02:19:55 -0500

    Fix CustomUser model for admin compatibility

--- create_superuser.py
+++ create_superuser.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')  # cambia 'tu_proyecto'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django.setup()
 
--- usuarios/models.py
+++ usuarios/models.py
@@ -3,12 +3,13 @@ from django.db import models
 import random
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, blank=True)
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=6, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     def generate_verification_code(self):
         self.verification_code = str(random.randint(100000, 999999))
